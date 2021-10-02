6bfec0adbe13dc8b04264bc21657fd9a6fc6bf85
areejsmac <joo123816@gmail.com>
2021-10-02 04:32:00 +0300

    new 2 in 1 app

---
+++ TowInOneApp2/app/src/main/java/com/example/towinoneapp2/GuessGameActivity.kt
@@ -0,0 +1,11 @@
+package com.example.towinoneapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuessGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guess_game)
+    }
+}
---
+++ TowInOneApp2/app/src/main/java/com/example/towinoneapp2/Itemes.kt
@@ -0,0 +1,5 @@
+package com.example.towinoneapp2
+
+class Itmes {
+
+}
